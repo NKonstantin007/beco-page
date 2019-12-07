eb0ace5b942b459cc1384c97d68942a958164373
kostya007 <kostya.1998.nosarev@mail.ru>
2019-12-07 11:45:55 +0700

    Reduce the width of seaech input when scrolinh

---
+++ script.js
@@ -0,0 +1,17 @@
+window.onscroll = function() {
+    const scroll = window.pageYOffset;
+    const searchInput = document.getElementsByClassName('search-input-wrapper')[0];
+    if(document.documentElement.clientWidth <= 320 && scroll > 16) {
+        searchInput.style.paddingLeft = '74px';
+    }
+    else {
+        searchInput.style.paddingLeft = '20px';
+    }
+
+    if(document.documentElement.clientWidth <= 768 && scroll > 20) {
+        searchInput.style.paddingLeft = '68px';
+    } 
+    else {
+        searchInput.style.paddingLeft = '26px';
+    }
+};
